df01fc1e9ff5d3451a6a3919583448f5a66c6536
KauaGabriell <kauagabriell64@gmail.com>
2026-06-09 11:45:13 -0300

    fix(auth): adjust role authorization

--- api/src/middlewares/verifyAuthorization.ts
+++ api/src/middlewares/verifyAuthorization.ts
@@ -2,7 +2,6 @@ import type { NextFunction, Request, Response } from "express";
 import { AppError } from "../utils/AppError";
 export function verifyAuthorization(roles: string[]) {
 	return (req: Request, _res: Response, next: NextFunction) => {
-		console.log(req.user);
 		if (!req.user) throw new AppError(401, "Não Autorizado");
 		if (!roles.includes(req.user?.role)) {
 			throw new AppError(403, "Não Autorizado");
