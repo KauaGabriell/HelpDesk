8a80b13ee06fcd5c1046b5b356ee82fc888a6710
KauaGabriell <kauagabriell64@gmail.com>
2026-06-09 09:32:08 -0300

    feat(api): add user authorization middleware

--- api/src/modules/auth/auth.routes.ts
+++ api/src/modules/auth/auth.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import { verifyAuthentication } from "../../middlewares/verifyAuthentication";
+import { verifyAuthorization } from "../../middlewares/verifyAuthorization";
 import { AuthController } from "./auth.controller";
 
 const authRoutes = Router();
 const authController = new AuthController();
 
 authRoutes.post("/login", authController.login);
+
+authRoutes.use(verifyAuthentication);
+authRoutes.use(verifyAuthorization(["client"]));
 authRoutes.post("/register", authController.register);
 
 export { authRoutes };
