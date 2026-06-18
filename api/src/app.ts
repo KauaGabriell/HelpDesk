29690d6bd7244f94284cdd6d44d65f81ac9e860e
KauaGabriell <kauagabriell64@gmail.com>
2026-06-18 13:55:36 -0300

    fix(upload): serve public upload files first

--- api/src/app.ts
+++ api/src/app.ts
@@ -7,8 +7,8 @@ import { routes } from "./routes";
 const app = express();
 app.use(express.json());
 
-app.use(routes);
 app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
+app.use(routes);
 
 app.use(errorHandling);
 
