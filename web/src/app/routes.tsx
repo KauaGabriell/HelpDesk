c3f836042b597b808791446f83d1dc6f1d6d6fc8
KauaGabriell <kauagabriell64@gmail.com>
2026-07-13 23:02:47 -0300

    feat(web): wire admin management routes

--- web/src/app/routes.tsx
+++ web/src/app/routes.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ProtectedRoute } from "../components/guards/ProtectedRoute";
 import { RoleRoute } from "../components/guards/RoleRoute";
-import { AdminTicketsPage } from "../modules/admin/pages/AdminTicketsPage";
+import { AdminClientsPage } from "../modules/admin/clients/pages/AdminClientsPage";
+import { AdminTechnicianFormPage } from "../modules/admin/technicians/pages/AdminTechnicianFormPage";
+import { AdminTechniciansPage } from "../modules/admin/technicians/pages/AdminTechniciansPage";
+import { AdminTicketDetailsPage } from "../modules/admin/tickets/pages/AdminTicketDetailsPage";
+import { AdminTicketsPage } from "../modules/admin/tickets/pages/AdminTicketsPage";
 import { LoginPage } from "../modules/auth/pages/LoginPage";
 import { ClientTicketsPage } from "../modules/client/pages/ClientTicketsPage";
 import { NotFoundPage } from "../modules/not-found/pages/NotFoundPage";
@@ -18,7 +22,24 @@ export function AppRoutes() {
 				<Route element={<ProtectedRoute />}>
 					<Route element={<AppLayout />}>
 						<Route element={<RoleRoute allowedRoles={["admin"]} />}>
+							<Route path="/admin/clients" element={<AdminClientsPage />} />
 							<Route path="/admin/tickets" element={<AdminTicketsPage />} />
+							<Route
+								path="/admin/tickets/:ticketId"
+								element={<AdminTicketDetailsPage />}
+							/>
+							<Route
+								path="/admin/technicians"
+								element={<AdminTechniciansPage />}
+							/>
+							<Route
+								path="/admin/technicians/new"
+								element={<AdminTechnicianFormPage mode="create" />}
+							/>
+							<Route
+								path="/admin/technicians/:technicianId/edit"
+								element={<AdminTechnicianFormPage mode="edit" />}
+							/>
 						</Route>
 
 						<Route element={<RoleRoute allowedRoles={["technician"]} />}>
